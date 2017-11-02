a71417bdfd8d2afb6bb3a18321cb693e585d2589
App Academy Student <student@appacademy.io>
2017-11-02 14:26:36 -0700

    add css friends4 and finished phase 1 of js

---
+++ skeleton/phase_1_arrays.js
@@ -0,0 +1,50 @@
+// Array#uniq - returns a new array containing only the unique elements of the original array
+// the unique elements should be in the order in which they first appear
+// should not mutate the original array
+// Array#twoSum - returns an array of position pairs where the elements sum to zero
+// Array#transpose - where we have a two-dimensional array representing a matrix. returns the transpose
+// should not mutate the original array
+
+
+Array.prototype.uniq = function () {
+  let count = {};
+  this.forEach( function(element) {
+    if (count[element] === undefined) {
+      count[element] = 1;
+    } else {
+      count[element] += 1;
+    }
+  });
+  let result = [];
+  for (var key in count) {
+    if (count[key] === 1) {
+      result.push(key);
+    }
+  }
+  return result;
+};
+
+Array.prototype.twoSum = function () {
+  let result = [];
+  for (var i = 0; i < this.length; i++){
+    for (var j = i+1; j < this.length; j++){
+      if (this[i] + this[j] === 0) {
+        result.push([i,j]);
+      }
+    }
+  }
+  return result;
+};
+
+
+Array.prototype.transpose = function () {
+  let result = [];
+  for (var i = 0; i < this.length; i++) {
+    let intResult = [];
+    for (var j = 0; j < this[0].length;j++){
+      intResult.push(this[j][i]);
+    }
+    result.push(intResult);
+  }
+  return result;
+};
